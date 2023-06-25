b3db29d7fa6b480ac1a341153a14121729371899
EC2 Default User <ec2-user@ip-10-0-0-31.ap-northeast-1.compute.internal>
2023-06-25 14:43:02 +0000

    add user destroy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
     def top
-
+     @posts=Post.all
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,10 @@ class UsersController < ApplicationController
         render :edit
      end
   end
+  def destroy
+    @user=User.find_by(id: params[:user_id]).destroy
+      redirect_to root_path
+  end
 
     private
     def user_params
